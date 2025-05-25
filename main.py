9fc6cf5d8f553cf45c778c86e3c9ca4fc7e879eb
Curtis-P <curtis.paff@gmail.com>
2025-05-25 04:39:50 -0400

    Refactored requests to print errors and added progress bar for data scraping

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import time
+from tqdm import tqdm
 
 
 def main():
     #get hmtl from homepage to start building URL
     base_url = "https://finder.cstone.space/"
-    base_page = requests.get(base_url) 
-    soup = BeautifulSoup(base_page.text, 'html.parser')
+    base_page = get_with_err_output(base_url)
+    soup = BeautifulSoup(base_page, 'html.parser')
 
     #Create url segment in line with website schema
     links = [] 
@@ -45,13 +46,12 @@ def main():
         "GetShipModule",
         "GetShipPaints",
     ]
-    
+
     #call api_endpoints to get url slugs, store it, create component entries, and store them for later use
     available_items = []
-    for index, endpoint in enumerate(api_endpoints):
+    for index, endpoint in enumerate(tqdm(api_endpoints, desc = "Generating available ship components...")):
         available_items.append(links[index] + " \n")
-        response = requests.get(base_url + endpoint)
-        data = response.json()
+        data = get_with_err_output(base_url + endpoint)
         for component in data:
             if component["Sold"] != 0:
                 component_id = component["ItemId"]
@@ -60,13 +60,13 @@ def main():
 
     #scrape hmtl from full url and add location field to items
     current_url = ""
-    for item in available_items:
+    for item in tqdm(available_items, desc="Scraping component location data..."):
         if isinstance(item, str):
             current_url = item[:-2]
             continue
         else:
-            current_page = requests.get(current_url + item['id']) 
-            current_page_html = BeautifulSoup(current_page.text, 'html.parser')
+            current_page = get_with_err_output(current_url + item['id']) 
+            current_page_html = BeautifulSoup(current_page, 'html.parser')
             desired_section = current_page_html.find('div', class_="pricetab")
             desired_section_rows = desired_section.find_all('tr')
 
@@ -78,10 +78,29 @@ def main():
                 cell_text = [cell.get_text(strip=True) for cell in data_cells]
                 seller_locations.append(cell_text[0])
             item['locations'] = tuple(seller_locations)
-            time.sleep(2)
+            time.sleep(1)
 
     df = pd.DataFrame(available_items)
     df.to_csv('ShipComponents.csv', index=False)
+
+
+#helper function to print our URL & error if requests fail.
+def get_with_err_output(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        if "Get" in url:
+            return response.json()
+        else:
+            return response.text
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Connection Error:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Something went wrong:", err)
 
 if __name__ == "__main__":
     main()    
